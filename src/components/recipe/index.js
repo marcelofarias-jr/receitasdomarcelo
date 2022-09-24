1fd29b16ca6deebb3f2f78baf61f6146b17c8db2
marcelofarias-jr <marcelobfariasjr@gmail.com>
2022-09-24 02:38:13 -0300

    Template de receita

--- src/components/recipe/index.js
+++ src/components/recipe/index.js
@@ -1,33 +1,41 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
-import { useParams } from "react-router-dom";
+import { isRouteErrorResponse, useParams } from "react-router-dom";
 import styles from "./Recipe.module.scss"
-import { loadTheRecipe } from "../../API/load-recipe";
 
 export default function PostRecipe() {
   const [item, setItem] = useState({});
-  const params = useParams();
+  const [igredientes, setIgredientes] = useState([]);
+  // const params = useParams();
   let { id } = useParams();
   console.log("id", id);
 
   const handleRevenue = (id) => {
     axios.get(`http://localhost:3000/receitas/` + id).then((response) => {
       const data = response.data;
-      console.log("data", data.titulo);
+      console.log("data", data);
       setItem(data);
     });
   };
   useEffect(() => {
     const data = handleRevenue(id);
-    console.log("item", item.titulo);
+    // setIgredientes(data.igredientes);
+    // const igredientes = setIgredientes(data.igredientes);
   }, []);
   return (
     <Container>
       <div className={styles.revenue}>
-        <img src={require("../../assets/img/" + item.foto + ".jpg")} />
         <h1>{item.titulo}</h1>
         <p>{item.resumo}</p>
+        <label>{item.tempoDePreparo}</label>
+        <label>{item.rendimento}</label>
+        <h3>Ingredientes</h3>
+        <ul>
+          {Map.(item.igredientes)}
+          <li>{item.igredientes[0]}</li>
+          <li>{item.igredientes[1]}</li>
+        </ul>
       </div>
     </Container>
   );
