61e1ff409cb91ad8bd505dda3b1af59fec57ca55
marcelofarias-jr <marcelobfariasjr@gmail.com>
2022-09-28 09:34:37 -0300

    Recipe single

--- src/components/recipe/index.js
+++ src/components/recipe/index.js
@@ -6,52 +6,67 @@ import { loadRecipe } from "../../API/load-recipe";
 
 export default function PostRecipe() {
   const [item, setItem] = useState([]);
-  const [Ingredients, setIngredients] = useState([]);
+  const [ingredients, setIngredients] = useState([]);
+  const [preparation, setPreparation] = useState([]);
 
   let { id } = useParams();
 
   const handleLoadPosts = useCallback(async (id) => {
-    const recipesResponse = await loadRecipe()
-    const idItem = (id-1)
+    const recipesResponse = await loadRecipe();
+    const idItem = id - 1;
     // const dataIgre = (recipesResponse.receitas[idItem].igredientes)
-    setIngredients(recipesResponse.receitas[idItem].igredientes)
-    setItem(recipesResponse.receitas[idItem])
+    setPreparation(recipesResponse.receitas[idItem].preparo);
+    setIngredients(recipesResponse.receitas[idItem].igredientes);
+    setItem(recipesResponse.receitas[idItem]);
   }, []);
 
   useEffect(() => {
-    handleLoadPosts(id)
-
+    handleLoadPosts(id);
   }, [id, handleLoadPosts]);
+  console.log("item", item);
+  console.log("preparation", preparation);
   return (
     <Container>
       <div className={styles.revenue}>
-        <h1>{item.titulo}</h1>
-        {!!item.foto && (
-          <img
-            src={require("../../assets/img/" + item.foto + ".jpg")}
-            alt={item.resumo}
-          />
-        )}
-
-        <p>{item.resumo}</p>
-        <div className={styles.tags}>
-          <label>{item.tempoDePreparo}</label>
-          <label>{item.rendimento}</label>
+        <div className={styles.revenue__center}>
+          <h1>{item.titulo}</h1>
+          {!!item.foto && (
+            <img
+              src={require("../../assets/img/" + item.foto + ".jpg")}
+              alt={item.resumo}
+            />
+          )}
+          <p>{item.resumo}</p>
         </div>
-
-        <h3>Ingredientes</h3>
-        <ul>
-        {Ingredients.map((item, i) =>{
-          return(
-            <li key={i}>{i+1}-{item}</li>
-          )
-
-        })}
-        </ul>
-
-
+        <div className={styles.revenue__left}>
+          <div className={styles.tags}>
+            <label>{item.tempoDePreparo}</label>
+            <label>{item.rendimento}</label>
+            <label>{item.tipo}</label>
+            {!!item.vegano && <label>Vegano</label>}
+          </div>
+          <h3>Ingredientes</h3>
+          <ul>
+            {ingredients.map((item, i) => {
+              return (
+                <li key={i}>
+                  {i + 1} - {item}
+                </li>
+              );
+            })}
+          </ul>
+          <h3>Modo de preparo</h3>
+          <ul>
+            {preparation.map((item, i) => {
+              return (
+                <li key={i}>
+                  {i + 1} - {item}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
     </Container>
   );
 }
-
