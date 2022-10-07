8f424687e4e39b0a50b9211a87e753d48494d69b
marcelofarias-jr <marcelobfariasjr@gmail.com>
2022-10-06 21:22:26 -0300

    .

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -8,81 +8,33 @@ import { loadRecipe } from "../../API/load-recipe";
 export default function Hero({data}) {
   const [index, setIndex] = useState(0);
   const [highlights, setHighlights] = useState([]);
-  const [dados, setDados] = useState([]);
+  const [dados, setDados] = useState([data]);
 
-
-  const handleLoadPosts = () => {
-    const recipesResponse =  loadRecipe();
-    setHighlights(recipesResponse.favoritos);
-    setDados(recipesResponse.receitas);
-  };
 
   const handleSelect = (selectedIndex, e) => {
     setIndex(selectedIndex);
   };
-  useEffect(() => {
-    handleLoadPosts();
-    console.log(dados)
-    // let resultado = dados.filter(h => h.id === (highlights[0].id))
-    // console.log('resultado', resultado);
-    // let teste = dados.filter(h => h.id === highlights[0].id);
-    // console.log('teste',teste);
 
-
-    // console.log('dados',dados);
-    // console.log('highlights',highlights)
-
-    // const [id] = highlights;
-    // console.log('id',id);
-
-
-  }, [dados]);
-  // console.log(highlights[0])
-  // id[0] = highlights[0]
-  // let teste = dados.filter(h => h.id === highlights[0].id);
-  // console.log('0', id[0])
-  // console.log('teste',teste[0].titulo);
+  console.log('dados',dados)
   return (
     <Container>
       <section className={styles.destaque}>
         <Carousel activeIndex={index} onSelect={handleSelect}>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src={require("../../assets/img/cake-g883e0c773_1920.jpg")}
-              alt="First slide"
-            />
-            <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src={require("../../assets/img/cake-g883e0c773_1920.jpg")}
-              alt="Second slide"
-            />
-
-            <Carousel.Caption>
-              <h3>Second slide label</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src={require("../../assets/img/pancakes-g02bec7b25_1920.jpg")}
-              alt="Second slide"
-            />
-
-            <Carousel.Caption>
-              <h3>Third slide label</h3>
-              <p>
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-              </p>
-            </Carousel.Caption>
-          </Carousel.Item>
+          {dados.map((receita)=>{
+            return(
+              <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={require("../../assets/img/"+receita.foto+".jpg")}
+                alt="First slide"
+              />
+              <Carousel.Caption>
+                <h3>{receita.titulo}</h3>
+                <p>{receita.resumo}</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            )
+          })}
         </Carousel>
       </section>
     </Container>
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -8,19 +8,24 @@ import styles from "./Home.module.scss"
 import { Contact } from "../../components/contact";
 export default function Home() {
 const [dados, setDados] = useState([]);
+const [fav, setFav] = useState([]);
 
 const handleLoadPosts = useCallback(async () => {
   const recipesResponse = await loadRecipe();
   setDados(recipesResponse.receitas);
+  let teste = dados.filter(x => x.destaque === true)
+  setFav(teste)
+
 }, [dados]);
 
   useEffect(() => {
     handleLoadPosts();
   }, [handleLoadPosts]);
-
+// console.log('home',dados)
+// console.log('teste',fav)
   return(
     <main className={styles.home}>
-        <Hero data={dados}/>
+        <Hero data={fav}/>
         <List data={dados}/>
         <Contact />
     </main>
